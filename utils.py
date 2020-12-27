1c73f025cd1fbee9474d5e6d1c47467c62f55fff
Viet Anh Nguyen <vietanhdev@gmail.com>
2020-12-27 11:11:18 +0700

    Image processing only

--- utils.py
+++ utils.py
@@ -1,157 +1,41 @@
-# Thêm thư viện
-import cv2, os
-import numpy as np
-import matplotlib.image as mpimg
+import cv2
+from pathlib import Path
+import os
 
-# Kích thước ảnh cho input của model
-IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS = 66, 200, 3
-INPUT_SHAPE = (IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)
+MAX_SPEED = 25
+MIN_SPEED = 10
+speed_limit = MAX_SPEED
 
+LOG_PATH = "logs"
 
-def load_image(data_dir, image_file):
+def init_log_dirs():
+    """Initialize log folder
     """
-    Đọc ảnh RGB từ file
+    Path(LOG_PATH).mkdir(parents=True, exist_ok=True)
+
+def write_image(image_type, frame_id, image):
+    """Write image to log dir
     """
-    return mpimg.imread(os.path.join(data_dir, image_file.strip()))
+    log_dir = os.path.join(LOG_PATH, image_type)
+    if not os.path.exists(log_dir):
+        Path(log_dir).mkdir(parents=True, exist_ok=True)
 
+    img_path = os.path.join(log_dir, "{}.png".format(frame_id))
+    cv2.imwrite(img_path, image)
 
-def crop(image):
-    """
-    Cắt bỏ bầu trời và mũi xe trong ảnh
-    """
-    return image[60:-25, :, :]
+def get_speed_limit(speed):
+    global speed_limit
+    if speed > speed_limit:
+        speed_limit = MIN_SPEED
+    else:
+        speed_limit = MAX_SPEED
+    return speed_limit
 
-
-def resize(image):
-    """
-    Resize ảnh
-    """
-    return cv2.resize(image, (IMAGE_WIDTH, IMAGE_HEIGHT), cv2.INTER_AREA)
-
-
-def rgb2yuv(image):
-    """
-    Chuyển ảnh RGB sang YUV
-    """
-    return cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
-
-
-def preprocess(image):
-    """
-    Pre-process ảnh
-    """
-    image = crop(image)
-    image = resize(image)
-    image = rgb2yuv(image)
-    return image
-
-
-def choose_image(data_dir, center, left, right, steering_angle):
-    """
-    Chọn ngẫu nhiên ảnh giữa, trái hoặc phải.
-    """
-    choice = np.random.choice(3)
-    if choice == 0:
-        return load_image(data_dir, left), steering_angle + 0.2
-    elif choice == 1:
-        return load_image(data_dir, right), steering_angle - 0.2
-    return load_image(data_dir, center), steering_angle
-
-
-def random_flip(image, steering_angle):
-    """
-    Ngẫu nhiên lật lại ảnh
-    """
-    if np.random.rand() < 0.5:
-        image = cv2.flip(image, 1)
-        steering_angle = -steering_angle
-    return image, steering_angle
-
-
-def random_translate(image, steering_angle, range_x, range_y):
-    """
-    Ngẫu nhiên dịch chuyển ảnh theo range_x, range_y.
-    """
-    trans_x = range_x * (np.random.rand() - 0.5)
-    trans_y = range_y * (np.random.rand() - 0.5)
-    steering_angle += trans_x * 0.002
-    trans_m = np.float32([[1, 0, trans_x], [0, 1, trans_y]])
-    height, width = image.shape[:2]
-    image = cv2.warpAffine(image, trans_m, (width, height))
-    return image, steering_angle
-
-
-def random_shadow(image):
-    """
-    Thêm giá trị tối cho ảnh
-    """
-    # (x1, y1) and (x2, y2) forms a line
-    # xm, ym gives all the locations of the image
-    x1, y1 = IMAGE_WIDTH * np.random.rand(), 0
-    x2, y2 = IMAGE_WIDTH * np.random.rand(), IMAGE_HEIGHT
-    xm, ym = np.mgrid[0:image.shape[0], 0:image.shape[1]]
-
-    # mathematically speaking, we want to set 1 below the line and zero otherwise
-    # Our coordinate is up side down.  So, the above the line: 
-    # (ym-y1)/(xm-x1) > (y2-y1)/(x2-x1)
-    # as x2 == x1 causes zero-division problem, we'll write it in the below form:
-    # (ym-y1)*(x2-x1) - (y2-y1)*(xm-x1) > 0
-    mask = np.zeros_like(image[:, :, 1])
-    mask[(ym - y1) * (x2 - x1) - (y2 - y1) * (xm - x1) > 0] = 1
-
-    # choose which side should have shadow and adjust saturation
-    cond = mask == np.random.randint(2)
-    s_ratio = np.random.uniform(low=0.2, high=0.5)
-
-    # adjust Saturation in HLS(Hue, Light, Saturation)
-    hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
-    hls[:, :, 1][cond] = hls[:, :, 1][cond] * s_ratio
-    return cv2.cvtColor(hls, cv2.COLOR_HLS2RGB)
-
-
-def random_brightness(image):
-    """
-    Thêm giá trị sáng cho ảnh.
-    """
-    # HSV (Hue, Saturation, Value) is also called HSB ('B' for Brightness).
-    hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-    ratio = 1.0 + 0.4 * (np.random.rand() - 0.5)
-    hsv[:,:,2] =  hsv[:,:,2] * ratio
-    return cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
-
-
-def augument(data_dir, center, left, right, steering_angle, range_x=100, range_y=10):
-    """
-    Kết hợp các phương pháp augmentation ảnh
-    """
-    image, steering_angle = choose_image(data_dir, center, left, right, steering_angle)
-    image, steering_angle = random_flip(image, steering_angle)
-    image, steering_angle = random_translate(image, steering_angle, range_x, range_y)
-    image = random_shadow(image)
-    image = random_brightness(image)
-    return image, steering_angle
-
-
-def batch_generator(data_dir, image_paths, steering_angles, batch_size, is_training):
-    """
-    Trả về ảnh và góc lái tương ứng cho việc training
-    """
-    images = np.empty([batch_size, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS])
-    steers = np.empty(batch_size)
-    while True:
-        i = 0
-        for index in np.random.permutation(image_paths.shape[0]):
-            center, left, right = image_paths[index]
-            steering_angle = steering_angles[index]
-            # argumentation
-            if is_training and np.random.rand() < 0.6:
-                image, steering_angle = augument(data_dir, center, left, right, steering_angle)
-            else:
-                image = load_image(data_dir, center) 
-            # add the image and steering angle to the batch
-            images[i] = preprocess(image)
-            steers[i] = steering_angle
-            i += 1
-            if i == batch_size:
-                break
-        yield images, steers
+def send_control(sio, steering_angle, throttle):
+    sio.emit(
+        "steer",
+        data={
+            'steering_angle': steering_angle.__str__(),
+            'throttle': throttle.__str__()
+        },
+        skip_sid=True)
