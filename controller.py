59614278b5b61b8061968e704994febe034e216e
Viet Anh Nguyen <vietanh.dev@gmail.com>
2021-10-23 23:57:52 +0700

    Minimize code

--- controller.py
+++ controller.py
@@ -1,40 +1,33 @@
 import cv2
 import numpy as np
-from utils import *
-from image_stream import image_streamer
 
-def calculate_control_signal(image):
-    steering_angle = 0
-    left_point, right_point, im_center = find_lane_lines(image)
-    if left_point != -1 and right_point != -1:
+def find_lane_lines(img):
+    """Phát hiện vạch kẻ đường
+    Hàm này sẽ nhận vào một hình ảnh màu, ở hệ màu BGR,
+    trả ra hình ảnh các vạch kẻ đường đã được lọc
+    """
 
-        # Calculate difference between car center point and image center point
-        center_point = (right_point + left_point) // 2
-        center_diff =  center_point - im_center
+    # Chuyển ảnh đã đọc sang ảnh xám
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        # Calculate steering angle from center point difference
-        steering_angle = float(center_diff * 0.01)
+    # Áp dụng bộ lọc Gaussian để loại bỏ bớt nhiễu.
+    # Các bạn có thể thử nghiệm các bộ lọc khác tại đây,
+    # như bộ lọc Median hoặc Bilateral.
+    img_gauss = cv2.GaussianBlur(gray, (11, 11), 0)
 
-    # Constant throttle = 0.5 * MAX SPEED
-    throttle = 0.5
+    # Áp dụng bộ lọc Canny với 2 ngưỡng.
+    # Các bạn có thể điều chỉnh 2 ngưỡng này và xem sự thay đổi
+    # trong ảnh kết quả
+    thresh_low = 150
+    thresh_high = 200
+    img_canny = cv2.Canny(img_gauss, thresh_low, thresh_high)
 
-    return throttle, steering_angle
-
-
-def grayscale(img):
-    """Convert image to grayscale"""
-    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-def canny(img, low_threshold, high_threshold):
-    """Apply the Canny transform"""
-    return cv2.Canny(img, low_threshold, high_threshold)
-
-def gaussian_blur(img, kernel_size):
-    """Apply a Gaussian Noise kernel"""
-    return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
+    # Trả về kết quả vạch kẻ đường
+    return img_canny
 
 def birdview_transform(img):
-    """Apply birdview transform"""
+    """Áp dụng chuyển đổi birdview
+    """
     IMAGE_H = 480
     IMAGE_W = 640
     src = np.float32([[0, IMAGE_H], [640, IMAGE_H], [0, IMAGE_H * 0.4], [IMAGE_W, IMAGE_H * 0.4]])
@@ -44,29 +37,14 @@ def birdview_transform(img):
     return warped_img
 
 
-def preprocess(img):
-    
-    img = grayscale(img)
-    image_streamer.set_image("grayscale", img)
-
-    img = gaussian_blur(img, 11)
-    image_streamer.set_image("gaussian_blur", img)
-
-    img = canny(img, 150, 200)
-    image_streamer.set_image("canny", img)
-
-    img = birdview_transform(img)
-    image_streamer.set_image("birdview", img)
-
-    return img
-
-
-def find_lane_lines(image, draw=False):
-
-    image = preprocess(image)
+def find_left_right_points(image, draw=False):
+    """Tìm điểm trái/phải
+    Sau khi có được ảnh birdview, một phương pháp đơn giản giúp xác định vị trí xe
+    so với làn đường là tìm 2 điểm, một điểm thuộc vạch kẻ đường bên trái và một điểm
+    thuộc vạch kẻ đường bên phải, sau cùng xét vị trí của điểm giữa xe so với hai điểm đó.
+    """
 
     im_height, im_width = image.shape[:2]
-
     if draw: viz_img = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
 
     # Vạch kẻ sử dụng để xác định tâm đường
@@ -78,8 +56,9 @@ def find_lane_lines(image, draw=False):
     left_point = -1
     right_point = -1
     lane_width = 100
-
     center = im_width // 2
+
+    # Tìm điểm bên trái và bên phải bằng cách duyệt từ tâm ra
     for x in range(center, 0, -1):
         if interested_line[x] > 0:
             left_point = x
@@ -88,20 +67,55 @@ def find_lane_lines(image, draw=False):
         if interested_line[x] > 0:
             right_point = x
             break
-    
-    # Dự đoán điểm trái và điểm phải nếu chỉ thấy 1 trong 2 điểm
+
+    # Dự đoán điểm bên phải khi chỉ nhìn thấy điểm bên trái
     if left_point != -1 and right_point == -1:
         right_point = left_point + lane_width
-    if left_point == -1 and right_point != -1:
+
+    # Dự đoán điểm bên trái khi chỉ thấy điểm bên phải
+    if right_point != -1 and left_point == -1:
         left_point = right_point - lane_width
 
-    if draw:
+    # Vẽ hai điểm trái / phải lên ảnh
+    if draw: 
         if left_point != -1:
             viz_img = cv2.circle(viz_img, (left_point, interested_line_y), 7, (255,255,0), -1)
         if right_point != -1:
             viz_img = cv2.circle(viz_img, (right_point, interested_line_y), 7, (0,255,0), -1)
 
-    if draw:
-        return left_point, right_point, center, viz_img
+    if draw: 
+        return left_point, right_point, viz_img
     else:
-        return left_point, right_point, center
+        return left_point, right_point
+
+
+def calculate_control_signal(img):
+    """Tính toán để tìm tốc độ và góc lái cho xe từ ảnh đầu vào
+    """
+
+    # Xử lý ảnh để tìm các điểm trái / phải
+    img_lines = find_lane_lines(img)
+    img_birdview = birdview_transform(img_lines)
+    left_point, right_point, viz_img = find_left_right_points(img_birdview, draw=True)
+
+    # Hiển thị kết quả xử lý ảnh
+    cv2.imshow("Result", viz_img)
+    cv2.waitKey(1)
+
+    # Tính toán tốc độ và góc lái
+    throttle = 0.5 # Tốc độ đang được đặt là 50% tốc độ cao nhất
+    steering_angle = 0
+    im_center = img.shape[1] // 2
+    # Nếu tìm thấy điểm trái và điểm phải,
+    # các điểm này sẽ có giá trị khác -1
+    if left_point != -1 and right_point != -1:
+
+        # Tính toán độ lệch giữa điểm giữa xe và làn đường
+        center_point = (right_point + left_point) // 2
+        center_diff =  im_center - center_point
+
+        # Tính toán góc lái tỷ lệ với độ lệch
+        steering_angle = - float(center_diff * 0.01)
+
+    return throttle, steering_angle
+
--- drive.py
+++ drive.py
@@ -1,107 +1,32 @@
-import base64
-import threading
-import time
-from io import BytesIO
-
-import cv2
-import eventlet
-import numpy as np
-import websocket
-from flask import Flask, jsonify, redirect, request, send_from_directory
-from flask_socketio import SocketIO
+import asyncio
+import websockets
 from PIL import Image
-from werkzeug import debug
+import json
+import cv2
+import numpy as np
+import base64
+from io import BytesIO
+from controller import calculate_control_signal
 
-from controller import *
-from image_stream import image_streamer
-from utils import *
+async def echo(websocket, path):
+    async for message in websocket:
+        # Nhận hình ảnh từ giả lập
+        data = json.loads(message)
+        image = Image.open(BytesIO(base64.b64decode(data["image"])))
+        image = np.asarray(image)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-eventlet.monkey_patch()
+        # Tính toán góc lái và tốc độ
+        throttle, steering_angle = calculate_control_signal(image.copy())
 
-app = Flask(__name__, static_url_path='')
-sio = SocketIO(app)
+        # Gửi tín hiệu điều khiển lên giả lập
+        message = json.dumps({"throttle": throttle, "steering": steering_angle})
+        print(message)
+        await websocket.send(message)
+        
 
+async def main():
+    async with websockets.serve(echo, "0.0.0.0", 4567, ping_interval=None):
+        await asyncio.Future()  # run forever
 
-def on_message(ws, message):
-    data = json.loads(message)
-    image = Image.open(BytesIO(base64.b64decode(data["image"])))
-    image = np.asarray(image)
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    image_streamer.set_image("rgb", image)
-
-    # Calculate speed and steering angle
-    throttle, steering_angle = calculate_control_signal(image.copy())
-    send_control(ws, steering_angle, throttle)
-
-
-@app.route('/')
-def homepage():
-    return redirect("/web/index.html?t={}".format(time.time()))
-
-@app.route('/web/<path:path>')
-def send_web(path):
-    return send_from_directory('web', path)
-
-@sio.on('connect')
-def connect():
-    send_control(sio, 0, 0)
-    print('[INFO] Client connected: {}'.format(request.sid))
-
-@sio.on('disconnect')
-def disconnect():
-    print('[INFO] Client disconnected: {}'.format(request.sid))
-
-@app.route('/api/get_topics')
-def get_topics():
-    return jsonify({
-        "success": True,
-        "topics": image_streamer.get_topics()
-    })
-
-@app.route('/api/set_topic')
-def set_topic():
-    topic = request.args.get("topic", "")
-    ret, message = image_streamer.set_current_topic(topic)
-    if ret:
-        return jsonify({
-            "success": True,
-            "new_topic": topic
-        })
-    else:
-        return jsonify({
-            "success": False,
-            "message": message
-        })
-
-# Start info streaming thread
-def info_thread_func(sio):
-    global count
-    while True:
-        sio.sleep(0.05)
-        frame = image_streamer.get_image(image_streamer.get_current_topic())
-        sio.emit(
-            'server2web',
-            {
-                'image': convert_image_to_jpeg(frame),
-                'topic': image_streamer.get_current_topic()
-            },
-            skip_sid=True, broadcast=True)
-info_thread = threading.Thread(target=info_thread_func, args=(sio,))
-info_thread.setDaemon(True)
-info_thread.start()
-
-
-def websocket_client_func():
-    global on_message
-    def on_error(ws, error):
-        print(error)
-    ws = websocket.WebSocketApp("ws://127.0.0.1:4567/simulation",
-    on_message=on_message,
-    on_error=on_error)
-    ws.run_forever()
-websocket_client_thread = threading.Thread(target=websocket_client_func)
-websocket_client_thread.setDaemon(True)
-websocket_client_thread.start()
-
-print("Starting server. Go to: http://localhost:8080")
-sio.run(app, port=8080)
+asyncio.run(main())
